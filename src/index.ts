786a47270d59b3e7f66d2fab4d656ba296a03f49
m-ghozi <ghozi286@gmail.com>
2026-06-19 09:26:49 +0700

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,11 @@ import customerRoutes from './routes/customer.routes';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(helmet());
 
+app.set('trust proxy', true);
 
 // Rate Limiting
 app.use(
